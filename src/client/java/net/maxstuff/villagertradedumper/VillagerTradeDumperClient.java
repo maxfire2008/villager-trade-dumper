83b58c42d3259dc70c02b99f3930782693212272
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-10-29 15:20:20 +1100

    Add new files and configurations for VillagerTradeDumper mod

---
+++ src/client/java/net/maxstuff/villagertradedumper/VillagerTradeDumperClient.java
@@ -0,0 +1,10 @@
+package net.maxstuff.villagertradedumper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillagerTradeDumperClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

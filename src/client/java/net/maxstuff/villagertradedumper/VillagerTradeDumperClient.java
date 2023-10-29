871ff7590e78e26afb7d8cc58ebc5e9014b8eba1
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-10-29 18:12:17 +1100

    Refactor VillagerTradeDumperClient to improve code readability and remove unnecessary code

--- src/client/java/net/maxstuff/villagertradedumper/VillagerTradeDumperClient.java
+++ src/client/java/net/maxstuff/villagertradedumper/VillagerTradeDumperClient.java
@@ -2,7 +2,6 @@ package net.maxstuff.villagertradedumper;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.village.TradeOfferList;
 import net.minecraft.text.Text;
@@ -14,12 +13,16 @@ import java.io.IOException;
 public class VillagerTradeDumperClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		System.out.println("VillagerTradeDumperClient loaded");
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+
+		// Register the onOpenScreen method to be called when a Screen
+		// is opened.
 		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
 			if (screen instanceof MerchantScreen) {
-				dumpTrades((MerchantScreen) screen);
+				System.out.println("MerchantScreen opened");
 			}
 		});
+		System.out.println("VillagerTradeDumperClient loaded");
 	}
 
 	private static void dumpTrades(MerchantScreen merchantScreen) {
@@ -27,13 +30,10 @@ public class VillagerTradeDumperClient implements ClientModInitializer {
 		MerchantScreenHandler merchantScreenHandler = (MerchantScreenHandler) merchantScreen.getScreenHandler();
 		TradeOfferList tradeOfferList = merchantScreenHandler.getRecipes();
 
-		// list the trades in the console and to the chat and to C:\tmp\trades.txt
+		// list the trades in the console and to C:\tmp\trades.txt
 		System.out.println("Trades:");
-		MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("Trades:"));
 		for (int i = 0; i < tradeOfferList.size(); i++) {
 			System.out.println("Trade " + i + ": " + tradeOfferList.get(i).toString());
-			MinecraftClient.getInstance().inGameHud.getChatHud()
-					.addMessage(Text.of("Trade " + i + ": " + tradeOfferList.get(i).toString()));
 		}
 		try {
 			FileWriter fileWriter = new FileWriter("C:\\tmp\\trades.txt");

46c4cb910b99d8803a85a7df4096e71d8ef6902c
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-10-29 17:21:57 +1100

    Refactored VillagerTradeDumperClient to use ScreenEvents and added dumpTrades method

--- src/client/java/net/maxstuff/villagertradedumper/VillagerTradeDumperClient.java
+++ src/client/java/net/maxstuff/villagertradedumper/VillagerTradeDumperClient.java
@@ -1,63 +1,49 @@
 package net.maxstuff.villagertradedumper;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
-import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.village.TradeOffer;
-import net.minecraft.village.TraderInventory;
-import net.minecraft.village.VillagerData;
-import net.minecraft.village.VillagerProfession;
+import net.minecraft.village.TradeOfferList;
+import net.minecraft.text.Text;
+import net.minecraft.screen.MerchantScreenHandler;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 public class VillagerTradeDumperClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as
-		// rendering.
-		MerchantScreen merchantScreen = (MerchantScreen) Screens.getScreen();
-		if (merchantScreen != null) {
-			VillagerEntity villager = merchantScreen.getMerchant();
-			if (villager instanceof TraderInventory) {
-				// Check if the "dump" button is pressed and log trades.
-				if (merchantScreen.getDumpButton().isMouseOver(merchantScreen.client.mouse.getX(),
-						merchantScreen.client.mouse.getY())) {
-					logTrades(villager);
-				}
+		System.out.println("VillagerTradeDumperClient loaded");
+		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+			if (screen instanceof MerchantScreen) {
+				dumpTrades((MerchantScreen) screen);
 			}
-		}
-
-		MerchantScreenHooks.registerTradeDumpButton();
+		});
 	}
 
-}
+	private static void dumpTrades(MerchantScreen merchantScreen) {
+		// use the mixed in MerchantScreen.getOffers() to get the TradeOfferList
+		MerchantScreenHandler merchantScreenHandler = (MerchantScreenHandler) merchantScreen.getScreenHandler();
+		TradeOfferList tradeOfferList = merchantScreenHandler.getRecipes();
 
-private void logTrades(VillagerEntity villager) {
-    List<TradeOffer> tradeOffers = new ArrayList<>();
-    VillagerData villagerData = villager.getVillagerData();
-    VillagerProfession profession = villagerData.getProfession();
-
-    for (TradeOffer trade : villager.getOffers()) {
-        tradeOffers.add(trade);
-    }
-
-    // Create a file and write the trade data to it.
-    Path logFilePath = MinecraftClient.getInstance().runDirectory.toPath().resolve("trade_log.txt");
-    try (FileWriter writer = new FileWriter(logFilePath.toFile(), true)) {
-        writer.write("Villager: " + profession.toString() + "\n");
-        for (TradeOffer tradeOffer : tradeOffers) {
-            writer.write("Input: " + tradeOffer.getOriginalFirstBuyItem().getTranslationKey() + "\n");
-            writer.write("Output: " + tradeOffer.getOriginalSecondBuyItem().getTranslationKey() + "\n");
-            writer.write("\n");
-        }
-        writer.close();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+		// list the trades in the console and to the chat and to C:\tmp\trades.txt
+		System.out.println("Trades:");
+		MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("Trades:"));
+		for (int i = 0; i < tradeOfferList.size(); i++) {
+			System.out.println("Trade " + i + ": " + tradeOfferList.get(i).toString());
+			MinecraftClient.getInstance().inGameHud.getChatHud()
+					.addMessage(Text.of("Trade " + i + ": " + tradeOfferList.get(i).toString()));
+		}
+		try {
+			FileWriter fileWriter = new FileWriter("C:\\tmp\\trades.txt");
+			fileWriter.write("Trades:\n");
+			for (int i = 0; i < tradeOfferList.size(); i++) {
+				fileWriter.write("Trade " + i + ": " + tradeOfferList.get(i).toString() + "\n");
+			}
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
